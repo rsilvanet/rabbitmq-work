9905f614781fc69f2dd292bab93ea52213bb57f3
Robson <me@rsilva.net>
2019-04-15 23:41:59 -0300

    Client basic code added

--- src/RabbitMQ.Work.Client/Program.cs
+++ src/RabbitMQ.Work.Client/Program.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace RabbitMQ.Work.Client
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("test", false, false, false);
+
+                    var message = Encoding.UTF8.GetBytes("Hello!");
+
+                    channel.BasicPublish("", "test", null, message);
+
+                    Console.WriteLine("Message sent!");
+                    Console.Read();
+                }
+            }
         }
     }
 }
