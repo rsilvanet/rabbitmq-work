05a43a5e3dc9e518f7b19990132eda4ce1ca5d30
Robson <me@rsilva.net>
2019-04-15 23:48:48 -0300

    Basic server code added

--- src/RabbitMQ.Work.Server/Program.cs
+++ src/RabbitMQ.Work.Server/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
 namespace RabbitMQ.Work.Server
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("test", false, false, false);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        Console.WriteLine("Received: {0}", Encoding.UTF8.GetString(ea.Body));
+                    };
+
+                    channel.BasicConsume("test", true, consumer);
+
+                    Console.Read();
+                }
+            }
         }
     }
 }
